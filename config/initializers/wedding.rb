892b173020cc1016bcc7d021306f75166b628add
Gulbir Pannu <singh7887@gmail.com>
2026-05-28 18:09:54 -0400

    Add hotel-booking CTA to RSVP confirmation; replace Provence leftovers

--- config/initializers/wedding.rb
+++ config/initializers/wedding.rb
@@ -3,5 +3,6 @@ WEDDING = {
   partner_1: "Nuvdeep Kaur Dhillon",
   partner_2: "Gulbir Singh Pannu",
   couple_names_possessive: "Nuvdeep & Gulbir's",
-  from_email: "Nuvdeep & Gulbir <gspannu@gmail.com>"
+  from_email: "Nuvdeep & Gulbir <gspannu@gmail.com>",
+  hotel_block_url: "https://book.passkey.com/gt/221257326?gtid=fe7103d4b4b8388a3467fc3555334171&mobile=true&dw=440"
 }.freeze
